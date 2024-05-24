27133fa261fc014be501f6a21bfa12dff4c9c089
mdalmamunit427 <mdalmamunit427@gmail.com>
2024-05-24 11:33:46 +0600

    mongoose connected

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+
+import config from "./app/config";
+import app from "./app";
+import mongoose from 'mongoose';
+
+async function main() {
+    try {
+        await mongoose.connect(config.db_url as string);
+
+    app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
+      })
+    } catch (error) {
+        console.error(`Server error: ${error}`)
+    }
+  
+  }
+
+main().then(()=> console.log("Mongodb connected successfully!")).catch(()=> console.log("Error connecting to mongodb"))
